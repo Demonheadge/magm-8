0e36c53d098730cca2dcd7af672588be9b746e24
Petuuuhhh <peterswanton191@bellsouth.net>
2023-12-29 15:00:14 -0500

    Try to port CFRU type bars

--- include/graphics.h
+++ include/graphics.h
@@ -3523,4 +3523,7 @@ extern const u8 gMailTilemap_Retro[];
 extern const u8 gMonMarkingsMenu_Gfx[];
 extern const u16 gMonMarkingsMenu_Pal[];
 
+extern const u8 CamomonsTypeIconsTiles[];
+extern const u8 CamomonsTypeIcons2Tiles[];
+
 #endif //GUARD_GRAPHICS_H
--- src/graphics.c
+++ src/graphics.c
@@ -2032,3 +2032,6 @@ const u16 gFrontierPassCancelButtonHighlighted_Tilemap[] = INCBIN_U16("graphics/
 const u16 gBerryCrush_Crusher_Pal[] = INCBIN_U16("graphics/berry_crush/crusher.gbapal");
 const u32 gBerryCrush_Crusher_Gfx[] = INCBIN_U32("graphics/berry_crush/crusher.4bpp.lz");
 const u32 gBerryCrush_TextWindows_Tilemap[] = INCBIN_U32("graphics/berry_crush/text_windows.bin.lz");
+
+const u8 CamomonsTypeIconsTiles[] = INCBIN_U8("graphics/types/CamomonsTypeIcons.4bpp.lz");
+const u8 CamomonsTypeIcons2Tiles[] = INCBIN_U8("graphics/types/CamomonsTypeIcons2.4bpp.lz");
