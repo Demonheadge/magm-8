ce953ec21dd6224c47a1352f88d90b8054b4a0f6
Petuuuhhh <peterswanton191@bellsouth.net>
2023-12-30 13:41:16 -0500

    Sprite converter (PokeScape Showdown)

---
+++ docs/Sprite_Converter/sprite_converter.py
@@ -0,0 +1,36 @@
+import os, sys, glob
+from PIL import Image, ImageSequence
+images = glob.glob("pokescape_monsters/*/back.png")
+master_width = 96
+master_height = 96
+background = Image.new(
+        mode='RGBA',
+        size=(master_width, master_height),
+        color=(0,0,0,0))
+background.save('background.png')
+background = Image.open("background.png")
+for filename in images:
+    master = Image.new(
+        mode='RGBA',
+        size=(master_width, master_height),
+        color=(0,0,0,0))
+    image = Image.open(filename)
+    width, height = image.size
+    master.paste(image,(round((master_width - width)/2),round((master_height - height)/2)))
+    # frames = []
+    # frame_copy = image.copy()
+    # for frame in ImageSequence.Iterator(image):
+        # output = background.copy()
+        # frame_px = frame.load()
+        # output_px = output.load()
+        # transparent_foreground = frame.convert('RGBA')
+        # transparent_foreground_px = transparent_foreground.load()
+        # for x in range(frame.width):
+            # for y in range(frame.height):
+                # output_px[round((master_width - width)/2) + x, round((master_height - height)/2) + y] = transparent_foreground_px[x, y]
+        # frames.append(output)
+    # frames.append(frame_copy)
+    # if '-front' not in filename and '-old' not in filename:
+        # frames[0].save(filename.lower().replace('.gif', '') + '-front.gif', save_all=True, append_images=frames[:-1], duration=50, disposal=2)
+    master.save(filename.replace(filename, filename.replace('pokescape_monsters\\', '').replace('\\back.png', '.png')).lower().replace('_', ''))
+    # print("\t'" + filename.replace('avatars\\', '').lower().replace('.png', '') + "-prism',")
